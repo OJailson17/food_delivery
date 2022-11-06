451f7d4c92615fc038247d34b31140c84a1df59c
OJailson17 <JaylsonO17@gmail.com>
2022-11-06 07:47:47 -0300

    feat: create sign in with form data

---
+++ src/pages/api/auth/login.ts
@@ -0,0 +1,66 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { fauna } from '../../../lib/faunadb';
+import { query as q } from 'faunadb';
+import { setCookie } from '../../../utils/cookies';
+
+interface User {
+	id: string;
+	name: string;
+	email: string;
+	password: string;
+	image: string;
+}
+
+interface SavedUserResponse {
+	data: User;
+}
+
+const SignIn = async (req: NextApiRequest, res: NextApiResponse) => {
+	const { email, password } = req.body;
+
+	console.table(req.body);
+
+	try {
+		// Check if email and password are correct, if not throw an error
+		const savedUser: SavedUserResponse = await fauna.query(
+			q.If(
+				q.Not(q.Exists(q.Match(q.Index('user_by_email'), q.Casefold(email)))),
+				q.Abort('User or password incorrect'),
+				q.If(
+					q.Exists(q.Match(q.Index('user_by_password'), password)),
+					q.Get(q.Match(q.Index('user_by_email'), q.Casefold(email))),
+					q.Abort('User or password incorrect'),
+				),
+			),
+		);
+
+		console.log({ savedUser });
+
+		// all user info
+		const cookiesData = savedUser.data;
+
+		// Stringify the cookie data object
+		const cookieString = JSON.stringify(cookiesData);
+
+		// Calling our pure function using the `res` object, it will add the `set-cookie` header
+		// Add the `set-cookie` header on the main domain and expire after 30 days
+		setCookie(res, 'user', cookieString, {
+			path: '/',
+			maxAge: 2592000,
+		});
+
+		// Return the `set-cookie` header so we can display it in the browser and show that it works!
+		res.getHeader('Set-Cookie');
+
+		return res.status(200).json({
+			success: true,
+			user: savedUser,
+		});
+	} catch (error) {
+		return res.json({
+			error,
+		});
+	}
+};
+
+export default SignIn;
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -2,8 +2,11 @@ import { GetServerSideProps } from 'next';
 import { signIn, getSession } from 'next-auth/react';
 import Image from 'next/image';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { useForm } from 'react-hook-form';
 
 import logoImg from '../../assets/food_delivery_logo.svg';
+import { api } from '../../lib/axios';
 import { GoogleButton } from '../../styles/common';
 import {
 	LoginContentContainer,
@@ -12,7 +15,37 @@ import {
 	LoginMainContainer,
 } from './styles';
 
+interface IFormReturn {
+	email: string;
+	password: string;
+}
+
 const Login = () => {
+	const { register, handleSubmit } = useForm<IFormReturn>();
+
+	const router = useRouter();
+
+	const handleSignIn = async (data: IFormReturn) => {
+		console.log(data);
+
+		try {
+			const response = await api.post('/api/auth/login', data);
+			console.log(response.data);
+
+			// TODO create a toast to show the error
+			if (response.data?.error?.description === 'User already exists') {
+				alert('Usuário já existe');
+				router.push('/login');
+			}
+
+			if (response.data.success) {
+				router.push('/');
+			}
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
 	return (
 		<LoginMainContainer>
 			<LoginContentContainer>
@@ -21,15 +54,23 @@ const Login = () => {
 				<LoginFormContainer>
 					<h2>Faça login</h2>
 
-					<LoginForm>
+					<LoginForm onSubmit={handleSubmit(handleSignIn)}>
 						<div>
 							<label htmlFor='email'>Email</label>
-							<input type='email' placeholder='Exemplo: john.doe@exemplo.com' />
+							<input
+								type='email'
+								placeholder='Exemplo: john.doe@exemplo.com'
+								{...register('email')}
+							/>
 						</div>
 
 						<div>
 							<label htmlFor='password'>Senha</label>
-							<input type='password' placeholder='No mínimo 6 caracteres' />
+							<input
+								type='password'
+								placeholder='No mínimo 6 caracteres'
+								{...register('password')}
+							/>
 						</div>
 
 						<button type='submit'>Entrar</button>
@@ -61,8 +102,18 @@ export const getServerSideProps: GetServerSideProps = async ({ req }) => {
 	// get the user session
 	const session = await getSession({ req });
 
+	let userCookies;
+
+	if (req.cookies.user) {
+		userCookies = JSON.parse(String(req.cookies.user));
+	}
+
+	console.log(session);
+
+	console.log(req.cookies);
+
 	// If the user is already authenticated, redirect it to the home page
-	if (session?.token.email) {
+	if (session || userCookies) {
 		return {
 			redirect: {
 				destination: '/',
