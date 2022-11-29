6270f5bcb7cb321c51af8341f750f23ec9b0ce79
OJailson17 <JaylsonO17@gmail.com>
2022-11-28 21:44:15 -0300

    fix: change axios url to prod

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const api = axios.create({
-	baseURL: `${process.env.NEXT_PUBLIC_AXIOS_BASE_URL_DEV}`,
+	baseURL: `${process.env.NEXT_PUBLIC_AXIOS_BASE_URL_PROD}`,
 });
